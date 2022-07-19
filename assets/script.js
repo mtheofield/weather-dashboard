7e07334001fe46a5579e4fa87f9120191ebacc07
Mary Theofield <mptheofield@gmail.com>
2022-07-19 08:50:09 -0400

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,138 @@
+var searchedCity = document.getElementById('searchedCity');
+var citySearched = document.getElementById('city-searched');
+let searchHistory = [];
+
+
+var formSubmitHandler = (event) => {
+    event.preventDefault();
+    let citiesName = searchedCity.value.trim();
+    if (citiesName) {
+        queryWeatherData(citiesName);
+        citiesName.value = '';
+    } else {
+        alert('Enter a city name');
+    }
+    citySearched.reset();
+};
+
+function queryWeatherData(citiesName) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citiesName}&units=imperial&appid=8bc0c59529456976b25bd3e2e58f2ccd`)
+    .then(response => {
+        return response.json();
+    })
+    .then(cityData => {
+        if (cityData.message === "please enter a city") {
+            alert("Enter a city");
+            return;
+        };
+        saveSearchHistory(citiesName);
+
+        var latitude = cityData.coord.lat
+        var longitude = cityData.coord.lon
+        var weatherIcon = cityData.weather[0].icon
+        var weatherTypeIcon = document.getElementById('weatherTypeIcon');
+        
+        weatherTypeIcon.setAttribute('src', `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`)
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=minutely,hourly,alerts&appid=8bc0c59529456976b25bd3e2e58f2ccd`)
+        .then(weatherData => {
+            return weatherData.json();
+        })
+        .then(weatherData => {
+            fiveDayforecast(weatherData)
+            var date = new Date(weatherData.current.dt * 1000);
+            var currentDate = Intl.DateTimeFormat().format(date);
+
+            var UVI = Math.round(weatherData.current.uvi);
+
+            if (UVI < 3) {
+                document.getElementById('UVI').classList.remove("moderateRiskUVI", "moderateHighRiskUVI", "highRiskUVI", "extremeRiskUVI");
+                document.getElementById('UVI').classList.add("lowRiskUVI");
+            } else if (UVI >=3 && UVI < 5) {
+                document.getElementById('UVI').classList.remove("lowRiskUVI", "moderateHighRiskUVI", "highRiskUVI", "extremelRiskUVI");
+                document.getElementById('UVI').classList.add("moderateRiskUVI");
+            } else if (UVI >=5 && UVI < 7) {
+                document.getElementById('UVI').classList.remove("lowRiskUVI", "moderateRiskUVI", "highRiskUVI", "extremelRiskUVI");
+                document.getElementById('UVI').classList.add("moderateHighRiskUVI");
+            } else if (UVI >=7 && UVI <= 10) {
+                document.getElementById('UVI').classList.remove("lowRiskUVI", "moderateRiskUVI", "moderateHighRiskUVI","extremelRiskUVI");
+                document.getElementById('UVI').classList.add("highRiskUVI");
+            } else {
+                document.getElementById('UVI').classList.remove("lowRiskUVI", "moderateRiskUVI", "moderateHighRiskUVI", "highRiskUVI");
+                document.getElementById('UVI').classList.add("extremelRiskUVI");
+            }
+
+            document.getElementById('searchedCityName').innerHTML = '' + citiesName + currentDate + '';
+            document.getElementById('humidity').innerHTML = 'Humidity: ' + weatherData.current.humidity + '%';
+            document.getElementById('wind').innerHTML = 'Wind: ' + weatherData.current.wind_speed + ' MPH';
+            document.getElementById('temperature').innerHTML = 'Temperature: ' + Math.round(weatherData.current.temp) + '°F';
+            document.getElementById('UVI').innerHTML = 'UV Index: ' + weatherData.current.uvi;
+
+            getSearchHistory();
+        })
+    });
+};
+function getSearchHistory() {
+    let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+    $('#searchHistoryBtns').empty();
+    if (searchHistory.length > 0) {
+        for(let i = 0; i < searchHistory.length; i++) {
+            let historyItem = $('<button>').attr('class', 'btn btn-secondary searchHistoryBtns').text(searchHistory[i]);
+            $('#searchHistoryBtns').append(historyItem);
+        }
+    }
+};
+getSearchHistory();
+
+
+function saveSearchHistory(citiesName) {
+    searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+    if (!searchHistory.includes(citiesName)) {
+        searchHistory.push(citiesName);
+    }
+
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+};
+
+
+citySearched.addEventListener('submit', formSubmitHandler);
+
+
+$(document).on('click', '.searchHistoryBtns', function(onClick) {
+    onClick.preventDefault();
+    this.value = '';
+    var  displayClickedCity = this.textContent;
+    queryWeatherData(displayClickedCity);
+});
+function fiveDayforecast(forecast) {
+    $('#fiveDayContainer').empty();
+    for (let i = 0; i < 5; i++) {
+        displayDailyforecast(forecast.daily[i]);
+    }
+};
+
+function displayDailyforecast(fiveDayData) {
+    var date = Intl.DateTimeFormat('en-US').format(new Date(fiveDayData.dt * 1000));
+    var weatherTypeIcon = fiveDayData.weather[0].icon;
+    var Temperature = fiveDayData.temp.day;
+    var Humidity = fiveDayData.humidity;
+    var Wind = fiveDayData.wind_speed;
+
+    var dailyCard = `
+        <div class="column col s12 m6">
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    <h4 class="list-group-item date">${date}</h4>
+                    <img class="list-group-item weather-icon" src="https://openweathermap.org/img/wn/${weatherTypeIcon}@2x.png" alt="weather icon">
+                    <li class="list-group-item temp">Temperature: ${Math.round(Temperature)}°F </li>
+                    <li class="list-group-item wind">Wind Speed: ${Wind} MPH</li>
+                    <li class="list-group-item humidity">Humidity: ${Humidity}% </li>
+                </ul>
+            </div>
+        </div>
+    `;
+
+    $('#fiveDayContainer').append(dailyCard);
+};
